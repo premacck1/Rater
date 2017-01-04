defea94f2ef7203fb6daa37f39da2216ed111775
Prem Ankur <premankur19@gmail.com>
2017-01-04 03:17:23 -0500

    Added RateSubjects.html and getSubjectsOnly.php

--- getSubjetsOnly.php
+++ getSubjetsOnly.php
@@ -11,12 +11,12 @@
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	} 
-	$sql = "SELECT a.subject_name as subject_name,AVG(a.avgRate) as avgRate,b.sem FROM quessubz a Inner JOIN subjects b on a.subject_name = b.subject_name where b.sem='".$_POST["semSelect"]."' group by a.subject_name";
+	$sql = "SELECT a.subject_name as subject_name, b.sem FROM quessubz a Inner JOIN subjects b on a.subject_name = b.subject_name where b.sem='".$_POST["semSelect"]."' group by a.subject_name";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-			echo "<tr> <td><a href=\"view.php?subject_name=".$row["subject_name"]."\">".$row["subject_name"]."</td><td>".$row["avgRate"]."</td></tr>";
+			echo "<tr> <td><a href=\"review.php?subject_name=".$row["subject_name"]."\">".$row["subject_name"]."</td></tr>";
 		}
 	} else {
 		echo "0 results";
